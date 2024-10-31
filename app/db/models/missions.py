fa457fd0cd7d74bc18a113e7be648e0cd1b3c94f
eytan zichel <eytanzichel@gmail.com>
2024-10-31 11:01:23 +0200

    missions model

---
+++ app/db/models/missions.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Numeric, Date
+from app.db import Base
+from sqlalchemy.orm import relationship
+
+
+class Missions(Base):
+    __tablename__ = 'missions'
+    mission_id = Column(Integer, nullable=False, autoincrement=True)
+    mission_date = Column(Date)
+    airborne_aircraft = Column(Numeric)
+    attacking_aircraft = Column(Numeric)
+    bombing_aircraft = Column(Numeric)
+    aircraft_returned = Column(Numeric)
+    aircraft_failed = Column(Numeric)
+    aircraft_damaged = Column(Numeric)
+    aircraft_lost = Column(Numeric)
+
